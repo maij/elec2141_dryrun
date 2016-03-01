18b3fa25c2c7bb87d5a55c71028a8d1f25dbab39
Mark Johnson <mark.johnson.ai@gmail.com>
2016-03-01 13:16:20 +1100

    Added simulation to the script, which works, although I still need to ensure that it terminates.
    Caveat:
    To use this the following environment variables have to be initialised to the following for Windows systems:
    
    XILINX = C:\path\to\Xilinx\ISE
    LD_LIBRARY_PATH = %XILINX%
    PLATFORM = nt
    
    And add the following to the PATH
    %XILINX%\lib\%PLATFORM%
    %XILINX%\bin\%PLATFORM%
    
    Note that only the 32 bit version currently works, 64 bit version will crash when trying to simulate.

--- dryrun.py
+++ dryrun.py
@@ -27,20 +27,50 @@ print(
 )
 tool_path = r"C:/Xilinx/14.4/ISE_DS/ISE/bin/nt64/"
 
-command_arguments = {"fuse.exe" : "-intstyle ise -incremental -lib unisims_ver -lib unimacro_ver -lib xilinxcorelib_ver -o {output} -prj {prj} work.{top_level} work.glbl"}
+command_arguments = {
+                    "fuse.exe" : '-intstyle ise -incremental -lib unisims_ver -lib unimacro_ver -lib xilinxcorelib_ver -o {output} -prj {prj} -top "work.{top_level}" -top "work.glbl"',
+                    "isim"     : '-intstyle ise -gui -tclbatch isim.cmd -wdb {top_level}.wdb'
+                    }
 ordered_commands = ["fuse.exe"]
 
 
 # Run fuse.exe first to compile and elaborate the design
+print(
+"""
+
+#####################################
+Compiling and elaborating all designs
+#####################################
+"""
+)
 command = "fuse.exe"
 for prj_file in project_files:
     unit_name = prj_file.strip(".prj")
     print("Processing project: " + unit_name + "\n")
 
     output_path = unit_name + "_isim.exe"
-    print(tool_path + command + " " + command_arguments[command].format(prj = prj_file, output = output_path, top_level = unit_name))
+    print("Running " + tool_path + command + " " + command_arguments[command].format(prj = prj_file, output = output_path, top_level = unit_name))
     os.system(tool_path + command + " " + command_arguments[command].format(prj = prj_file, output = output_path, top_level = unit_name))
         
+# Run each isim wrapper to simulate each design
+print(
+"""
+
+#####################################
+       Simulating all designs
+#####################################
+"""
+)
+
+command = "isim"
+for sim in glob("*_isim.exe"):
+    unit_name = sim.strip(".exe")
+    print("Simulating project: " + unit_name + "\n")
+
+    print("Running " + sim + " " + command_arguments[command].format(top_level = unit_name))
+    os.system(         sim + " " + command_arguments[command].format(top_level = unit_name))
+
+    
 # Finished, wait on input
 print("Done, press enter to terminate")
 sys.stdin.readline()
