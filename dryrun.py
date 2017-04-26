4ef15966b9e16b2211ba58d37ad1f82576310c2f
Mark Johnson <mark.johnson.ai@gmail.com>
2017-04-26 10:26:38 +1000

    Tidy up comments, raise error for failed imports.

--- dryrun.py
+++ dryrun.py
@@ -5,7 +5,7 @@ from glob import glob
 
 # Just a simple "new line" function
 def nl():
-    print("\n")
+    print("")
 
 def about_message():
     return \
@@ -28,7 +28,7 @@ def usage():
     ./dryrun.py lab2
     """
     
-# To help with booking showing which version of python is being used
+# To help with book-keeping, show which version of python is being used
 print(sys.version + "\n")
 
 # Exit with usage message if no arguments given
@@ -36,19 +36,22 @@ if len(sys.argv) <= 1: sys.exit(usage())
 
 # Use the correct test bench for the current run
 lab = sys.argv[1]
-if   (lab == "lab1"): import conf.lab1 as lab_module
-elif (lab == "lab2"): import conf.lab2 as lab_module
-elif (lab == "lab3"): import conf.lab3 as lab_module
-elif (lab == "lab4"): import conf.lab4 as lab_module
-elif (lab == "lab5"): import conf.lab5 as lab_module
-elif (lab == "lab6"): import conf.lab6 as lab_module
-elif (lab == "lab7"): import conf.lab7 as lab_module
-elif (lab == "lab8"): import conf.lab8 as lab_module
-else                : sys.exit("ERROR : argument must be one of lab[1-8]\n\n" + usage())
+try:
+    if   (lab == "lab1"): import conf.lab1 as lab_module
+    elif (lab == "lab2"): import conf.lab2 as lab_module
+    elif (lab == "lab3"): import conf.lab3 as lab_module
+    elif (lab == "lab4"): import conf.lab4 as lab_module
+    elif (lab == "lab5"): import conf.lab5 as lab_module
+    elif (lab == "lab6"): import conf.lab6 as lab_module
+    elif (lab == "lab7"): import conf.lab7 as lab_module
+    elif (lab == "lab8"): import conf.lab8 as lab_module
+    else                : sys.exit("ERROR : argument must be one of lab[1-8]\n\n" + usage())
+except:
+    raise ImportError('Could not import configuration for specified lab: \'{}\''.format(lab))
 
 test_bench_file = lab_module.test_bench_file
 
-# After all the setup has completed successfully, print the header and 
+# After all the setup has completed successfully, print the header and...
 print(about_message())
 
 # Environment variables used during simulation
@@ -61,8 +64,6 @@ env_vars = {
 for env in env_vars.keys():
     print("{env: <16} := {value}".format(env = env, value = env_vars[env]))
     os.environ[env] = env_vars[env]
-#for env in os.environ.keys():
-#    print("{env: <16} := {value}".format(env = env, value = os.environ[env]))
 
 project_files = glob("*.prj")
 nl()
